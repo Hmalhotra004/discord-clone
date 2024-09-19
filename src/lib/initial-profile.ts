2980cb50ce4243287dd3cc705628f347bb5245e4
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-09-19 15:21:41 +0530

    initial profile page more components

---
+++ src/lib/initial-profile.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+const initialProfile = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return RedirectToSignIn;
+  }
+
+  const profile = await db.profile.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (profile) {
+    return profile;
+  }
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+
+  return newProfile;
+};
+
+export default initialProfile;
