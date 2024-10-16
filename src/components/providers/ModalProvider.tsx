fd1095cc96d044f47e3c505b5b4687f4f2072e3c
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-10-17 00:21:29 +0530

    members modal half

--- src/components/providers/ModalProvider.tsx
+++ src/components/providers/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import CreateServerModal from "@/components/modals/CreateServerModal";
 import EditServerModal from "@/components/modals/EditServerModal";
 import InviteModal from "@/components/modals/InviteModal";
+import MembersModal from "@/components/modals/MembersModal";
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -21,6 +22,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
